fc1e25f47708bfcd4cf94324dafc7cde3dd1e65c
Pedro Marques <roque@juniper.net>
2014-05-29 18:35:26 -0700

    Create the skeleton for the service-instance unit test

---
+++ src/vnsw/agent/oper/service_instance.cc
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include "service_instance.h"
+#include "ifmap/ifmap_node.h"
+#include "schema/vnc_cfg_types.h"
+
+using boost::uuids::uuid;
+
+class ServiceInstanceKey : public AgentKey {
+  public:
+    ServiceInstanceKey(uuid uuid) {
+        uuid_ = uuid;
+    }
+    const uuid &instance_id() const {
+        return uuid_;
+    }
+
+  private:
+    uuid uuid_;
+};
+
+class ServiceInstanceData : public AgentData {
+  public:
+    ServiceInstanceData(const autogen::ServiceInstanceType &data) :
+            data_(data) {
+    }
+  private:
+    autogen::ServiceInstanceType data_;
+};
+
+static uuid IdPermsGetUuid(const autogen::IdPermsType &id) {
+    uuid uuid;
+    CfgUuidSet(id.uuid.uuid_mslong, id.uuid.uuid_lslong, uuid);
+    return uuid;
+}
+
+/*
+ * ServiceInstance class
+ */
+ServiceInstance::ServiceInstance()
+        : service_type_(0), virtualization_type_(0) {
+}
+
+bool ServiceInstance::IsLess(const DBEntry &rhs) const {
+    const ServiceInstance &si = static_cast<const ServiceInstance &>(rhs);
+    return uuid_ < si.uuid_;
+}
+
+std::string ServiceInstance::ToString() const {
+    std::stringstream uuid_str;
+    uuid_str << uuid_;
+    return uuid_str.str();
+}
+
+void ServiceInstance::SetKey(const DBRequestKey *key) {
+    const ServiceInstanceKey *si_key =
+            static_cast<const ServiceInstanceKey *>(key);
+    uuid_ = si_key->instance_id();
+}
+
+DBEntryBase::KeyPtr ServiceInstance::GetDBRequestKey() const {
+    ServiceInstanceKey *key = new ServiceInstanceKey(uuid_);
+    return KeyPtr(key);
+}
+
+bool ServiceInstance::DBEntrySandesh(Sandesh *sresp, std::string &name) const {
+    return false;
+}
+
+/*
+ * ServiceInstanceTable class
+ */
+ServiceInstanceTable::ServiceInstanceTable(DB *db, const std::string &name)
+        : AgentDBTable(db, name) {
+}
+
+std::auto_ptr<DBEntry> ServiceInstanceTable::AllocEntry(
+    const DBRequestKey *key) const {
+    std::auto_ptr<DBEntry> entry(new ServiceInstance());
+    entry->SetKey(key);
+    return entry;
+}
+
+bool ServiceInstanceTable::IFNodeToReq(IFMapNode *node, DBRequest &req) {
+    autogen::ServiceInstance *svc_instance =
+            static_cast<autogen::ServiceInstance *>(node->GetObject());
+    autogen::IdPermsType id = svc_instance->id_perms();
+    req.key.reset(new ServiceInstanceKey(IdPermsGetUuid(id)));
+    if (!node->IsDeleted()) {
+        req.oper = DBRequest::DB_ENTRY_ADD_CHANGE;
+        req.data.reset(new ServiceInstanceData(svc_instance->properties()));
+    } else {
+        req.oper = DBRequest::DB_ENTRY_DELETE;
+    }
+    return true;
+}
+
+ 
+DBTableBase *ServiceInstanceTable::CreateTable(
+    DB *db, const std::string &name) {
+    ServiceInstanceTable *table = new ServiceInstanceTable(db, name);
+    table->Init();
+    return table;
+}
