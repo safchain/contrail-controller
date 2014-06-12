21196d7c5e37d905f9f88e4765f708d9cba57f16
Pedro Marques <roque@juniper.net>
2014-06-11 18:30:51 -0700

    Get first integration test for service-instance properties to work
    Change-Id: I1c5dda3f580e9593abe3656b2951298904a7f906

--- src/vnsw/agent/test/service_instance_test.cc
+++ src/vnsw/agent/test/service_instance_test.cc
@@ -15,6 +15,12 @@
 
 using boost::uuids::uuid;
 
+static std::string uuid_to_string(const uuid &uuid) {
+    std::stringstream sgen;
+    sgen << uuid;
+    return sgen.str();
+}
+
 static void UuidTypeSet(const uuid &uuid, autogen::UuidType *idpair) {
     idpair->uuid_lslong = 0;
     idpair->uuid_mslong = 0;
@@ -40,6 +46,11 @@ class ServiceInstanceIntegrationTest : public ::testing::Test {
         agent_->CreateDBTables();
         agent_->CreateDBClients();
         agent_->InitModules();
+        MessageInit();
+    }
+
+    void MessageInit() {
+        doc_.reset();
         config_ = doc_.append_child("config");
     }
 
@@ -55,17 +66,126 @@ class ServiceInstanceIntegrationTest : public ::testing::Test {
         object->EncodeUpdate(&node);
     }
 
-    void EncodeServiceInstance(const uuid &uuid) {
+    void EncodeLink(pugi::xml_node *parent,
+                    const std::string &lhs_type,
+                    const std::string &lhs_name,
+                    const std::string &rhs_type,
+                    const std::string &rhs_name,
+                    const std::string &metadata) {
+        pugi::xml_node link = parent->append_child("link");
+        pugi::xml_node left = link.append_child("node");
+        left.append_attribute("type") = lhs_type.c_str();
+        left.append_child("name").text().set(lhs_name.c_str());
+        pugi::xml_node right = link.append_child("node");
+        right.append_attribute("type") = rhs_type.c_str();
+        right.append_child("name").text().set(rhs_name.c_str());
+        pugi::xml_node meta = link.append_child("metadata");
+        meta.append_attribute("type") = metadata.c_str();
+    }
+
+    void EncodeServiceInstance(const uuid &uuid, const std::string &name) {
         autogen::IdPermsType id;
         id.Clear();
         UuidTypeSet(uuid, &id.uuid);
         autogen::ServiceInstance svc_instance;
         svc_instance.SetProperty("id-perms", &id);
-        std::stringstream name_gen;
-        name_gen << "contrail:service-instance:" << uuid;
+
+        autogen::ServiceInstanceType svc_properties;
+        svc_properties.Clear();
+        svc_properties.left_virtual_network = "vnet-left";
+        svc_properties.right_virtual_network = "vnet-right";
+        svc_instance.SetProperty("service-instance-properties",
+                                 &svc_properties);
+
         pugi::xml_node update = config_.append_child("update");
-        EncodeNode(&update, "service-instance", name_gen.str(), &svc_instance);
+        EncodeNode(&update, "service-instance", name, &svc_instance);
     }
+
+    void ConnectServiceTemplate(const std::string &name) {
+        boost::uuids::random_generator gen;
+        autogen::IdPermsType id;
+        id.Clear();
+        UuidTypeSet(gen(), &id.uuid);
+
+        autogen::ServiceTemplate svc_template;
+        svc_template.SetProperty("id-perms", &id);
+
+        autogen::ServiceTemplateType props;
+        // TODO: set service-type and instance-type
+        props.Clear();
+
+        pugi::xml_node update = config_.append_child("update");
+        EncodeNode(&update, "service-template", name, &svc_template);
+
+        update = config_.append_child("update");
+        EncodeLink(&update,
+                   "service-instance", name,
+                   "service-template", name,
+                   "service-instance-service-template");
+    }
+
+    void ConnectVirtualMachine(const std::string &name) {
+        boost::uuids::random_generator gen;
+        autogen::IdPermsType id;
+        id.Clear();
+        UuidTypeSet(gen(), &id.uuid);
+
+        autogen::VirtualMachine virtual_machine;
+        virtual_machine.SetProperty("id-perms", &id);
+
+        pugi::xml_node update = config_.append_child("update");
+        EncodeNode(&update, "virtual-machine", name, &virtual_machine);
+
+        update = config_.append_child("update");
+        EncodeLink(&update,
+                   "virtual-machine", name,
+                   "service-instance", name,
+                   "virtual-machine-service-instance");
+    }
+
+    void ConnectVirtualNetwork(const std::string &vmi_name) {
+        boost::uuids::random_generator gen;
+        autogen::IdPermsType id;
+        id.Clear();
+        UuidTypeSet(gen(), &id.uuid);
+
+        autogen::VirtualNetwork vnet;
+        vnet.SetProperty("id-perms", &id);
+
+        pugi::xml_node update = config_.append_child("update");
+        std::string vnet_name("vnet-");
+        vnet_name.append(vmi_name);
+        EncodeNode(&update, "virtual-network", vnet_name, &vnet);
+
+        update = config_.append_child("update");
+        EncodeLink(&update,
+                   "virtual-machine-interface", vmi_name,
+                   "virtual-network", vnet_name,
+                   "virtual-machine-interface-virtual-network");
+    }
+
+    void ConnectVirtualMachineInterface(const std::string &vm_name,
+                                        const std::string &vmi_name) {
+        boost::uuids::random_generator gen;
+        autogen::IdPermsType id;
+        id.Clear();
+        UuidTypeSet(gen(), &id.uuid);
+
+        autogen::VirtualMachineInterface vmi;
+        vmi.SetProperty("id-perms", &id);
+
+        pugi::xml_node update = config_.append_child("update");
+        EncodeNode(&update, "virtual-machine-interface", vmi_name, &vmi);
+
+        ConnectVirtualNetwork(vmi_name);
+
+        update = config_.append_child("update");
+        EncodeLink(&update,
+                   "virtual-machine-interface", vmi_name,
+                   "virtual-machine", vm_name,
+                   "virtual-machine-interface-virtual-machine");
+    }
+
 
   protected:
     std::auto_ptr<Agent> agent_;
@@ -76,7 +196,7 @@ class ServiceInstanceIntegrationTest : public ::testing::Test {
 TEST_F(ServiceInstanceIntegrationTest, Config) {
     boost::uuids::random_generator gen;
     uuid svc_id = gen();
-    EncodeServiceInstance(svc_id);
+    EncodeServiceInstance(svc_id, "test-1");
     IFMapAgentParser *parser = agent_->GetIfMapAgentParser();
     parser->ConfigParse(config_, 1);
     task_util::WaitForIdle();
@@ -88,6 +208,22 @@ TEST_F(ServiceInstanceIntegrationTest, Config) {
     ServiceInstance *svc_instance =
             static_cast<ServiceInstance *>(si_table->Find(&key, true));
     ASSERT_TRUE(svc_instance != NULL);
+
+    MessageInit();
+    ConnectServiceTemplate("test-1");
+    parser->ConfigParse(config_, 1);
+    task_util::WaitForIdle();
+
+    MessageInit();
+    ConnectVirtualMachine("test-1");
+    ConnectVirtualMachineInterface("test-1", "left");
+    ConnectVirtualMachineInterface("test-1", "right");
+    parser->ConfigParse(config_, 1);
+    task_util::WaitForIdle();
+
+    EXPECT_FALSE(svc_instance->properties().instance_id.is_nil());
+    EXPECT_FALSE(svc_instance->properties().vmi_inside.is_nil());
+    EXPECT_FALSE(svc_instance->properties().vmi_outside.is_nil());
 }
 
 static void SetUp() {
